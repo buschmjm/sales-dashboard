4251ef6778c54618a2eb53cf4a06538b6fad8341
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2024-12-03 21:23:37 +0000

    Edited Custom.APICalls.GoTo

--- server_code/Custom/APICalls/GoTo.py
+++ server_code/Custom/APICalls/GoTo.py
@@ -21,7 +21,7 @@ import http.client
 @anvil.server.callable
 conn = http.client.HTTPSConnection("api.goto.com")
 
-conn.request("GET", "/call-reports/v1/reports/user-activity?organizationId=0127d974-f9f3-0704-2dee-000100422009&startTime=2020-01-01T00%3A01%3A00Z&endTime=2020-01-01T00%3A01%3A00Z&page=SOME_INTEGER_VALUE&pageSize=SOME_INTEGER_VALUE&q=SOME_ARRAY_VALUE&userIds=SOME_ARRAY_VALUE&sort=SOME_ARRAY_VALUE")
+conn.request("GET", "/call-reports/v1/reports/user-activity?organizationId=0127d974-f9f3-0704-2dee-000100422009")
 
 res = conn.getresponse()
 data = res.read()
