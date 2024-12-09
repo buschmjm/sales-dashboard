3020252701b751de14e7f070e183d29e79da4738
buschmann.jake@gmail.com <buschmann.jake@gmail.com>
2024-12-09 21:50:01 +0000

    Edited Frame

--- client_code/Frame/__init__.py
+++ client_code/Frame/__init__.py
@@ -8,50 +8,48 @@ from anvil.tables import app_tables
 from ..Reports import Reports
 from ..Sales import Sales
 
-#This is your startup form. It has a sidebar with navigation links and a content panel where page content will be added.
 class Frame(FrameTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    #Present users with a login form with just one line of code:
-    #anvil.users.login_with_form()
 
-    #Set the Plotly plots template to match the theme of the app
+    # Add the refresh button to the top-right of the form
+    self.refresh_button = Button(text="Refresh", align="right", role="primary-color")
+    self.refresh_button.set_event_handler("click", self.refresh_button_click)
+    self.add_component(self.refresh_button, slot="top-right")
+
+    # Set the Plotly plots template to match the theme of the app
     Plot.templates.default = "rally"
-    #When the app starts up, the Sales form will be added to the page
+
+    # When the app starts up, the Sales form will be added to the page
     self.content_panel.add_component(Sales())
-    #Change the color of the sales_page_link to indicate that the Sales page has been selected
+
+    # Change the color of the sales_page_link to indicate that the Sales page has been selected
     self.sales_page_link.background = app.theme_colors['Primary Container']
-    
+
+  def refresh_button_click(self, **event_args):
+    """This method is called when the refresh button is clicked"""
+    try:
+      # Call the fetch_call_reports function from the server
+      result = anvil.server.call('fetch_call_reports_requests')
+      alert("Data refreshed successfully!", title="Success")
+    except Exception as e:
+      alert(f"Failed to refresh data: {e}", title="Error")
 
   def sales_page_link_click(self, **event_args):
     """This method is called when the link is clicked"""
-    #Clear the content panel and add the Sales Form
+    # Clear the content panel and add the Sales Form
     self.content_panel.clear()
     self.content_panel.add_component(Sales())
-    #Change the color of the sales_page_link to indicate that the Sales page has been selected
+    # Change the color of the sales_page_link to indicate that the Sales page has been selected
     self.sales_page_link.background = app.theme_colors['Primary Container']
     self.reports_page_link.background = "transparent"
 
   def reports_page_link_click(self, **event_args):
     """This method is called when the link is clicked"""
-    #Clear the content panel and add the Reports Form
+    # Clear the content panel and add the Reports Form
     self.content_panel.clear()
     self.content_panel.add_component(Reports())
-    #Change the color of the sales_page_link to indicate that the Reports page has been selected
+    # Change the color of the sales_page_link to indicate that the Reports page has been selected
     self.reports_page_link.background = app.theme_colors['Primary Container']
     self.sales_page_link.background = "transparent"
-
-  #If using the Users service, uncomment this code to log out the user:
-  # def signout_link_click(self, **event_args):
-  #   """This method is called when the link is clicked"""
-  #   anvil.users.logout()
-  #   open_form('Logout')
-
-
-
-
-
-
-
-
