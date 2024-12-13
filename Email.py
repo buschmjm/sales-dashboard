1cf3f027946a96997c9b7f808726b937c6d26c57
Jacob Buschmann <buschmann.jake@gmail.com>
2024-12-13 13:17:51 -0600

    Add initial Email module for handling email-related functionality

---
+++ Email.py
@@ -0,0 +1,7 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.secrets
+import anvil.http
+import pytz
+from datetime import datetime
+from anvil.tables import app_tables  # Add this import
